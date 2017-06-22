e45b9d83d1b18bc648f792d6c915a81b10d3c519
carlganz <carlganz@gmail.com>
2017-06-22 17:59:21 +0000

    cdn parameter

--- R/include.R
+++ R/include.R
@@ -1,9 +1,27 @@
 #' Include JsBarcode JS Script
+#' @param cdn Indicate whether to get JsBarcode from CDN or server
 #' @importFrom shiny includeScript
 #' @export
 
-includeJsBarcode <- function() {
-  shiny::includeScript(
-    system.file(package = "CannaBarcode", "javascript", "JsBarcode", "JsBarcode.all.min.js")
-  )
+includeJsBarcode <- function(cdn = FALSE) {
+  if (cdn) {
+    shiny::tagList(
+      shiny::includeScript(
+        "https://cdn.jsdelivr.net/jsbarcode/3.5.8/JsBarcode.all.min.js"
+      ),
+      shiny::includeScript(
+        system.file(package = "CannaBarcode", "javascript",  "CannaBarcode.js")
+      )
+    )
+  } else {
+    shiny::tagList(
+      shiny::includeScript(
+        system.file(package = "CannaBarcode", "javascript", "JsBarcode", "JsBarcode.all.min.js")
+      ),
+      shiny::includeScript(
+        system.file(package = "CannaBarcode", "javascript",  "CannaBarcode.js")
+      )
+    )
+  }
+
 }
